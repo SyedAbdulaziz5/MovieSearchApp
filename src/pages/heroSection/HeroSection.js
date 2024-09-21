dd9cea78eff12d9717e1beeb333378ae1fd3ad4c
Syed Abdulaziz <syedabdulaziz@gmail.com>
2024-09-21 08:47:20 -0700

    added some features

--- src/pages/heroSection/HeroSection.js
+++ src/pages/heroSection/HeroSection.js
@@ -27,7 +27,7 @@ const Home = () => {
           {popularMovies.map((movie) => (
             <Link
               style={{ textDecoration: "none", color: "white" }}
-              to={`/movie/${movie.id}`}
+              // to={`/movie/${movie.id}`}
             >
               <div className="posterImage">
                 <img
