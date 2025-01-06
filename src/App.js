aad83f7bb5f6a3a702890b219c22e3803f4723a9
Syed Abdulaziz <syedabdulaziz531@gmail.com>
2025-01-07 00:21:24 +0500

    fix errors

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Header from "./components/Header/Header";
 import AppRoutes from "./routes";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Circle from "./components/Circle";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <AppRoutes />
-      {/* <Circle /> */}
       <Footer />
     </div>
   );
